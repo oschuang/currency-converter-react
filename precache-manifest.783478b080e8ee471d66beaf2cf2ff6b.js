6ee060dcd184472272399a763847a7179c0cea91
oschuang <oschuang1@gmail.com>
2020-03-31 11:46:07 -0400

    Updates

---
+++ precache-manifest.783478b080e8ee471d66beaf2cf2ff6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c253f9c0f90edd447b3a50ea634ea05d",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "a26ac7d7b3d42ea5d456",
+    "url": "/currency-converter-react/static/css/main.59ce7ef8.chunk.css"
+  },
+  {
+    "revision": "3d8cf20b738b98893e8c",
+    "url": "/currency-converter-react/static/js/2.c1a0d429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.c1a0d429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26ac7d7b3d42ea5d456",
+    "url": "/currency-converter-react/static/js/main.ae08dc7b.chunk.js"
+  },
+  {
+    "revision": "adaf8d0ea337b67b5550",
+    "url": "/currency-converter-react/static/js/runtime-main.d79a609a.js"
+  }
+]);
